7b2a9f31fbba71eb2b7a86e466c5fce88318592f
Yongmin <qwelllo@naver.com>
2023-10-01 19:10:40 +0900

    [5.5] listener로 DB 변경 감지와 반영

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import colors from "../colors";
-import { SafeAreaView } from "react-native";
+import { FlatList, SafeAreaView } from "react-native";
 import {Ionicons} from '@expo/vector-icons'
+import { useDB } from "../context";
 
 const View = styled.View`
     flex: 1;
@@ -30,11 +31,58 @@ const ButtonText = styled.Text`
     color: white;
 `;
 
+const Record = styled.View`
+    background-color: ${colors.cardColor};
+    flex-direction: row;
+    align-items: center;
+    padding: 10px 20px;
+    border-radius: 10px;
+`
+const Emotion = styled.Text`
+    font-size: 24px;
+    margin-right: 10px;
+`
+const Message = styled.Text`
+    font-size: 18px;
+    font-weight: 500;
+`;
+
+const Separator = styled.View`
+    height: 10px
+`;
+
 const Home = ({navigation: {navigate}}) => {
+    const realm = useDB();
+    const [feelings, setFeelings] = useState(realm.objects("Feeling"));
+
+    useEffect(()=> {
+        const feelings = realm.objects("Feeling");
+        setFeelings(feelings);
+        feelings.addListener(() => {
+            const feelings = realm.objects("Feeling");
+            setFeelings(feelings);
+        });
+
+        return () => { // useEffect에서 리턴되는 값은 component가 unmount 되었을 때 실행되는 함수를 리턴한다.
+            feelings.removeAllListeners();
+        }
+    },[])
+
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: colors.bgColor}}>
             <View>
                 <Title>My Diary</Title>
+                <FlatList
+                    data={feelings}
+                    keyExtractor={feeling => feeling._id + ""}
+                    renderItem={({item}) => (
+                        <Record>
+                            <Emotion>{item.emotion}</Emotion>
+                            <Message>{item.message}</Message>
+                        </Record>
+                    )}
+                    ItemSeparatorComponent={Separator}
+                />
                 <Button onPress={() => {navigate("Write")}}>
                     <Ionicons name={"add"} color={"white"} size={40}/>
                 </Button>
